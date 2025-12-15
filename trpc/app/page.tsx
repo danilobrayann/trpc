d1180941f9599e3337c1df0df29b8b9f3c773a46
danilobrayann <danilobrayann@gmail.com>
2025-12-15 18:49:43 -0300

    feat: Add user data fetching to Home component

--- trpc/app/page.tsx
+++ trpc/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { trpc } from "@/server/client";
+
 export default function Home() {
+  const getUser = trpc.user.getUsers.useQuery();
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h2>hello world</h2>
+      {JSON.stringify(getUser.data)}
     </div>
   );
 }
